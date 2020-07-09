92a38270aa7086d8c9ac46c3203c5052f094bdae
Felipe Fadul <felipe.fadul@hotmail.com>
2020-07-08 23:28:58 -0300

    feat: Altera link no título da loja
    Agora, ao clicar no link do título da loja no cabeçalho, redireciona
    para a tela Home.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,7 +6,7 @@ import Cart from '../Cart';
 export default function Header({ cart, clearCart }) {
   return (
     <S.HeaderContainer>
-      <S.StoreTitle to="/store" onClick={window.scrollTo(0, 0)}>
+      <S.StoreTitle to="/">
         PokeStore
       </S.StoreTitle>
       <Cart cart={ cart } clearCart={ clearCart } />
